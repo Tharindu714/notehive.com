d6a643b81954d6c25ea1b8e4eff8716e843f8ddd
Tharindu Chanaka <110843121+Tharindu714@users.noreply.github.com>
2025-07-19 13:56:04 +0530

    Update firebase.js

--- assets/js/firebase.js
+++ assets/js/firebase.js
@@ -258,19 +258,19 @@ document.getElementById("noteForm").addEventListener("submit", async (e) => {
   bootstrap.Modal.getInstance(document.getElementById("noteModal")).hide();
 });
 
-fetch("footer.html")
+fetch("./footer.html")
   .then((response) => response.text())
   .then((data) => {
     document.getElementById("footer-placeholder").innerHTML = data;
   });
 
-fetch("header.html")
+fetch("./header.html")
   .then((response) => response.text())
   .then((data) => {
     document.getElementById("header-placeholder").innerHTML = data;
   });
 
-fetch("slider-area.html")
+fetch("./slider-area.html")
   .then((response) => response.text())
   .then((data) => {
     document.getElementById("slider-placeholder").innerHTML = data;
@@ -318,7 +318,7 @@ document.addEventListener('click', e => {
 });
 
 
-fetch("service.html")
+fetch("./service.html")
   .then((response) => response.text())
   .then((data) => {
     document.getElementById("service-placeholder").innerHTML = data;
